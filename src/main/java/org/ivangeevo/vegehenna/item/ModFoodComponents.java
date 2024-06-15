57adcc4a0f14b417f0011c4179ddcbdaae2c54b3
Ivan Geevo <dbowgaming@gmail.com>
2024-06-15 19:45:14 +0300

    bump

--- src/main/java/org/ivangeevo/vegehenna/item/ModFoodComponents.java
+++ src/main/java/org/ivangeevo/vegehenna/item/ModFoodComponents.java
@@ -5,9 +5,6 @@ import net.minecraft.item.FoodComponent;
 /** Contains all the default food components used in Vegehenna food items. **/
 public class ModFoodComponents
 {
-    public static final FoodComponent CARROT =
-            new FoodComponent.Builder().hunger(1).saturationModifier(0.003f).build();
-
     public static final FoodComponent COOKED_CARROT =
             new FoodComponent.Builder().hunger(2).saturationModifier(0.005f).build();
 
---
+++ src/main/java/org/ivangeevo/vegehenna/mixin/SweetBerryBushBlockMixin.java
@@ -0,0 +1,61 @@
+package org.ivangeevo.vegehenna.mixin;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.PlantBlock;
+import net.minecraft.block.SweetBerryBushBlock;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.sound.SoundCategory;
+import net.minecraft.sound.SoundEvents;
+import net.minecraft.state.property.IntProperty;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
+import net.minecraft.util.hit.BlockHitResult;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import net.minecraft.world.event.GameEvent;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(SweetBerryBushBlock.class)
+public class SweetBerryBushBlockMixin extends PlantBlock
+{
+
+    @Shadow @Final public static IntProperty AGE;
+
+    public SweetBerryBushBlockMixin(Settings settings)
+    {
+        super(settings);
+    }
+
+    // Modify the drop to only 1 instead of a random like before.
+    @Inject(method = "onUse", at = @At("HEAD"), cancellable = true)
+    private void injectedOnUse(BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockHitResult hit, CallbackInfoReturnable<ActionResult> cir)
+    {
+        boolean bl;
+        int i = state.get(AGE);
+        bl = i == 3;
+
+        if (!bl && player.getStackInHand(hand).isOf(Items.BONE_MEAL))
+        {
+            cir.setReturnValue( ActionResult.PASS );
+        }
+
+        if (i > 1)
+        {
+            SweetBerryBushBlock.dropStack(world, pos, new ItemStack(Items.SWEET_BERRIES, 1));
+            world.playSound(null, pos, SoundEvents.BLOCK_SWEET_BERRY_BUSH_PICK_BERRIES, SoundCategory.BLOCKS, 1.0f, 0.8f + world.random.nextFloat() * 0.4f);
+            BlockState blockState = state.with(AGE, 1);
+            world.setBlockState(pos, blockState, Block.NOTIFY_LISTENERS);
+            world.emitGameEvent(GameEvent.BLOCK_CHANGE, pos, GameEvent.Emitter.of(player, blockState));
+            cir.setReturnValue( ActionResult.success(world.isClient) );
+        }
+        cir.setReturnValue( super.onUse(state, world, pos, player, hand, hit) );
+    }
+}
