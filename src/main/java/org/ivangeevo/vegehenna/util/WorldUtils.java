a1e0a0490c18411428db3f3ce2be46330b8b6874
Ivan Geevo <dbowgaming@gmail.com>
2024-10-25 19:43:58 +0300

    bump

--- src/main/java/org/ivangeevo/vegehenna/util/WorldUtils.java
+++ src/main/java/org/ivangeevo/vegehenna/util/WorldUtils.java
@@ -3,10 +3,7 @@
 package org.ivangeevo.vegehenna.util;
 
 
-import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.World;
 
 /**
  * Helper functions related to the world
